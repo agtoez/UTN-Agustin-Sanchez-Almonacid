ffc02f285d7bc7a14a74a6c5ae8e53bb608ddbda
agtoez <agtoez@gmail.com>
2025-09-13 02:47:59 -0300

    Hasta ej9, corregir salto de línea en ej8.

--- Estructuras repetitivas/estructuras_repetitivas.py
+++ Estructuras repetitivas/estructuras_repetitivas.py
@@ -13,6 +13,7 @@ entre = int(input("Ingrese el número desde el que quiere sumar(Este no se sumar
 hasta = int(input("Ingrese el número hasta el que quiere sumar(Este tampoco se sumará):"))
 total = 0
 for i in range(entre, hasta):
+    i+1
     total += i
 print(f"El total de la suma de los enteros entre: {entre}; hasta: {hasta}; es: {total}")
 # 4) Elabora un programa que permita al usuario ingresar números enteros y los sume en
@@ -39,14 +40,49 @@ while(nU != nAl):
 print(f"Has acertado, el número es: {nAl} y ha tomado: {acum} intentos")
 # 6) Desarrolla un programa que imprima en pantalla todos los números pares comprendidos
 # entre 0 y 100, en orden decreciente.
+for i in range(1, 102):
+    print(i - 1)
 # 7) Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un
 # número entero positivo indicado por el usuario.
+hta = int(input("Ingrese el número hasta el que quiere sumar:"))
+totalHta = 0
+for i in range(0, hta):
+    i + 1
+    totalHta += i
+print(f"El total de la suma de los enteros entre: 0; hasta: {hta}; es: {totalHta}")
 # 8) Escribe un programa que permita al usuario ingresar 100 números enteros. Luego, el
 # programa debe indicar cuántos de estos números son pares, cuántos son impares, cuántos son
 # negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad
 # menor, pero debe estar preparado para procesar 100 números con un solo cambio).
+numero = 0
+cantNumero = 0
+numPar = 0
+numImpar = 0
+numNeg = 0
+numPos = 0
+
+while(cantNumero <= 9):
+    numero = int(input("Ingrese un número entero: "))
+    if (numero > 0): numPos += 1
+    elif (numero < 0): numNeg += 1
+
+    if (numero % 2 == 0): numPar += 1
+    elif (numero % 2 != 0): numImpar += 1
+    cantNumero += 1
+print(f"Resultados: n\ Cantidad de números positivos: {numPos} n\ Cantidad de números negativos: {numNeg} n\ Cantidad de números pares: {numPar} n\ Cantidad de números impares: {numImpar}" )
+
 # 9) Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la
 # media de esos valores. (Nota: puedes probar el programa con una cantidad menor, pero debe
 # poder procesar 100 números cambiando solo un valor).
+tot9 = 0 
+cantidad = 10
+
+for i in range(cantidad):
+    n9 = int(input(f"Ingrese el número {i+1} de {cantidad}: "))
+    tot9 += n9
+
+media = tot9 / cantidad
+print(f"La media de los {cantidad} números ingresados es: {media}")
+
 # 10) Escribe un programa que invierta el orden de los dígitos de un número ingresado por el
 # usuario. Ejemplo: si el usuario ingresa 547, el programa debe mostrar 745.
