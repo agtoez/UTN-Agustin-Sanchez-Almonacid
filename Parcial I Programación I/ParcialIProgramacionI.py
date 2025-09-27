5ac752501ed7eab2a96fa2a1ca26f485741720fc
acidoalmendra <agustinsalmonacid@gmail.com>
2025-09-26 22:17:43 -0300

    Programa Parcial Programación I

--- Parcial I Programación I/ParcialIProgramacionI.py
+++ Parcial I Programación I/ParcialIProgramacionI.py
@@ -21,6 +21,41 @@
 # - Devolución -> Aumenta en 1 la cantidad de ejemplares del libro seleccionado.
 # 8. Salir → Termina la ejecución del programa.
 
+#Introducción al programa:
+print("||| Sistema de gestión de biblioteca escolar. |||")
 #Declaración de variables.
 titulos = []
 ejemplares = []
+
+while True:
+    print("1. Ingresar títulos")
+    print("2. Ingresar ejemplares")
+    print("3. Mostrar catálogo")
+    print("4. Consultar disponibilidad")
+    print("5. Listar agotados")
+    print("6. Agregar título")
+    print("7. Actualizar ejemplares (préstamo/devolución)")
+    print("8. Salir")
+
+    opcion = int(input("Ingrese el número de la opción: "))
+
+    if opcion == 1:
+        cantidad = int(input("¿Cuántos títulos desea ingresar?"))
+        for i in range(cantidad):
+            titulos.append(input("Ingrese el título: "))
+            ejemplares.append(0)
+
+    if opcion == 2:
+        for i in range(len(titulos)):
+            print("Título: ", titulos[i])
+            ejemplares[i] = int(input("Ingrese la cantidad de ejemplares: "))
+
+    if opcion == 3:
+        for i in range(len(titulos)):
+            print(f"Título: {titulos[i]}, Cantidad: {ejemplares[i]}")
+
+    if opcion == 4:
+    # if opcion == 5:
+    # if opcion == 6:1
+    # if opcion == 7:
+    # if opcion == 8:
