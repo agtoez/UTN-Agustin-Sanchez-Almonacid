285e24c398d9b0fc983cb8a55ba2fb1874a9c375
acidoalmendra <agustinsalmonacid@gmail.com>
2025-09-27 14:06:07 -0300

    Programa parcial I

--- Parcial I Programación I/ParcialIProgramacionI.py
+++ Parcial I Programación I/ParcialIProgramacionI.py
@@ -22,12 +22,15 @@
 # 8. Salir → Termina la ejecución del programa.
 
 #Introducción al programa:
-print("||| Sistema de gestión de biblioteca escolar. |||")
+print("\n" + "-"*50)
+print("||| Sistema de gestión de biblioteca escolar |||")
+print("-"*50)
 #Declaración de variables.
 titulos = []
 ejemplares = []
 
 while True:
+    print("\n" + "-"*50)
     print("1. Ingresar títulos")
     print("2. Ingresar ejemplares")
     print("3. Mostrar catálogo")
@@ -36,6 +39,7 @@ while True:
     print("6. Agregar título")
     print("7. Actualizar ejemplares (préstamo/devolución)")
     print("8. Salir")
+    print("-"*50)
 
     opcion = int(input("Ingrese el número de la opción: "))
 
@@ -45,17 +49,58 @@ while True:
             titulos.append(input("Ingrese el título: "))
             ejemplares.append(0)
 
-    if opcion == 2:
+    elif opcion == 2:
         for i in range(len(titulos)):
             print("Título: ", titulos[i])
             ejemplares[i] = int(input("Ingrese la cantidad de ejemplares: "))
 
-    if opcion == 3:
+    elif opcion == 3:
         for i in range(len(titulos)):
             print(f"Título: {titulos[i]}, Cantidad: {ejemplares[i]}")
 
-    if opcion == 4:
-    # if opcion == 5:
-    # if opcion == 6:1
-    # if opcion == 7:
-    # if opcion == 8:
+    elif opcion == 4:
+        busqueda = input("Ingrese el nombre del título para consultar disponibilidad: ")
+        if busqueda in titulos:
+            posicion = titulos.index(busqueda)
+            print(f"Hay {ejemplares[posicion]} ejemplares disponibles.")
+        else:
+            print("El título no existe.")
+
+    elif opcion == 5:
+        agotados = False
+        for i in range(len(ejemplares)):
+            if ejemplares[i] == 0:
+                print(f"{titulos[i]} está agotado.")
+                agotados = True
+        if not agotados:
+                print("Todos los títulos están disponibles.")
+
+    elif opcion == 6:
+        titulos.append(input("Ingrese el nuevo título: "))
+        ejemplares.append(int(input("Ingrese la cantidad de ejemplares: ")))
+
+    elif opcion == 7:
+        actualizacion = input("Ingrese el título a actualizar:")
+        if actualizacion in titulos:
+            posicion = titulos.index(actualizacion)
+            print("1. Préstamo")
+            print("2. Devolución")
+            subopcion = input("Ingrese 1 para préstamos y 2 para devoluciones.")
+            if subopcion == "1":
+                if ejemplares[posicion] > 0:
+                    ejemplares[posicion] -= 1
+                    print("Prestamo registrado.")
+                else:
+                    print("No hay ejemplares disponibles")
+            elif subopcion == "2":
+                ejemplares[posicion] += 1
+                print("Devolución registrada.")
+        else:
+            print("El título no existe en el catálogo.")
+
+    elif opcion == 8:
+        print("Gracias por utilizar el programa.")
+        break
+
+    else:
+        print("Opción inválida, intente de nuevo.")
